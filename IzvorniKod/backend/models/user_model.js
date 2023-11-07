938c3224ed5b8945be8b97a32bf50d56d3a26299
petarpandza <petarpandzapetar@gmail.com>
2023-11-07 21:42:15 +0100

    Database refactor, code tidying

--- IzvorniKod/backend/models/user_model.js
+++ IzvorniKod/backend/models/user_model.js
@@ -1,36 +1,52 @@
 const createUserTable = `
-    CREATE TABLE user
-    (
-        id INTEGER NOT NULL,
+    CREATE TABLE user (
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
         name TEXT NOT NULL,
         surname TEXT NOT NULL,
         email TEXT NOT NULL,
         password TEXT NOT NULL,
         is_admin INTEGER DEFAULT 0,
         token TEXT,
-        PRIMARY KEY (id),
         UNIQUE (email)
     );
     `;
 
-const createUser = `INSERT INTO user (name, surname, email, password) VALUES (?, ?, ?, ?)`;
-const createUserWithRole = `INSERT INTO user (name, surname, email, password, is_admin) VALUES (?, ?, ?, ?, ?)`;
+const createUser = `
+    INSERT INTO user (name, surname, email, password) VALUES (@name, @surname, @email, @password);
+`;
 
-const loginEmailPassword = `SELECT * FROM user WHERE email = ? AND password = ?`;
+const createAdmin = `
+    INSERT INTO user (name, surname, email, password, is_admin) VALUES (@name, @surname, @email, @password, 1);
+`;
 
-const getAllUsers = `SELECT * FROM user`;
+const loginEmailPassword = `
+    SELECT * FROM user WHERE email = @email AND password = @password;
+`;
 
-const getUserByToken = `SELECT * FROM user WHERE token = ?;`;
-const updateTokenByEmail = `UPDATE user SET token = ? WHERE email = ? RETURNING *;`;
+const getAllUsers = `
+    SELECT * FROM user;
+`;
+
+const getUserByToken = `
+    SELECT * FROM user WHERE token = @token;
+`;
+
+const updateTokenByEmail = `
+    UPDATE user SET token = @token WHERE email = @email RETURNING *;
+`;
+const updateTokenById = `
+    UPDATE user SET token = @token WHERE id = @id RETURNING *;
+`;
 
 module.exports = {
     createUserTable,
     createUser,
+    createAdmin,
     loginEmailPassword,
     getAllUsers,
     getUserByToken,
     updateTokenByEmail,
-    createUserWithRole
+    updateTokenById
 };
 
 
--- IzvorniKod/backend/models/word_model.js
+++ IzvorniKod/backend/models/word_model.js
@@ -1,24 +1,34 @@
 const db = require('../database/database');
 
 const createWordTable = `
-    CREATE TABLE word
-    (
-        id INT NOT NULL,
-        foreign_word INT NOT NULL,
-        foreign_description INT NOT NULL,
-        nativeWord INT NOT NULL,
-        nativeDescription INT NOT NULL,
-        pronounciation INT NOT NULL,
-        dictionary_id INT NOT NULL,
-        PRIMARY KEY (id),
+    CREATE TABLE word (
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        foreign_word TEXT NOT NULL,
+        foreign_description TEXT NOT NULL,
+        nativeWord TEXT NOT NULL,
+        nativeDescription TEXT NOT NULL,
+        pronounciation TEXT NOT NULL,
+        dictionary_id INTEGER NOT NULL,
         FOREIGN KEY (dictionary_id) REFERENCES dictionary(id),
-        UNIQUE (foreign_word),
-        UNIQUE (foreign_description),
-        UNIQUE (pronounciation),
-        UNIQUE (dictionary_id)
+        UNIQUE (foreign_word, foreign_description, nativeWord, nativeDescription, dictionary_id)
     );
-    `;
+`;
+
+const createWord = `
+    INSERT INTO word (foreign_word, foreign_description, nativeWord, nativeDescription, pronounciation, dictionary_id) VALUES (@foreignWord, @foreignDescription, @nativeWord, @nativeDescription, @pronounciation, @dictionaryId);
+`;
+
+const deleteWordById = `
+    DELETE FROM word WHERE id = @wordId;
+`;
+
+const deleteWordByMeaning = `
+    DELETE FROM word WHERE foreign_word = @foreignWord AND foreign_description = @foreignDescription AND nativeWord = @nativeWord AND nativeDescription = @nativeDescription;
+`;
 
 module.exports = {
-	createWordTable
+    createWordTable,
+    createWord,
+    deleteWordById,
+    deleteWordByMeaning
 }
