061a38756ef2dce091a16ef5a66608c51ebac516
Alberto Kerim <40634753+AlbertoKerim@users.noreply.github.com>
2023-12-12 16:42:57 +0100

    Dodana HTML stranica kada se šalje mail umjesto teksta

--- IzvorniKod/app/routes/mailScript.js
+++ IzvorniKod/app/routes/mailScript.js
@@ -25,7 +25,61 @@ router.post('/sendMessage', (req, res) => {
     from: 'balkanlingo@gmail.com',
     to: emailSend,
     subject: 'Password set',
-    text: 'Proba',
+    html: `
+    <html>
+        <head>
+          <style>
+            body {
+              font-family: 'Arial', sans-serif;
+              background-color: #f4f4f4;
+            }
+            .container {
+              max-width: 600px;
+              margin: 20px auto;
+              padding: 20px;
+              background-color: #fff;
+              border-radius: 10px;
+              box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            }
+            h1 {
+              color: #333;
+              text-align: center;
+            }
+            p {
+              color: #666;
+            }
+            h6 {
+              color: #4CA;
+            }
+            .button-container {
+              text-align: center;
+            }
+            .button {
+              display: inline-block;
+              padding: 10px 20px;
+              font-size: 16px;
+              text-align: center;
+              text-decoration: none;
+              background-color: #4CAF50;
+              color: #fff;
+              border-radius: 5px;
+              cursor: pointer;
+            }
+          </style>
+        </head>
+        <body>
+          <div class="container">
+            <h1>Vaša jednokratna lozinka</h1>
+            <p><strong>Username:</strong> ${name}</p>
+            <p><strong>Jednokratna lozinka:</strong> ${email}</p>
+            <div class="button-container"><a href="138.201.184.55:3000" class="button">Prijavi se</a></div>
+            <hr>
+            <p><strong>Molimo vas da se što prije se prijavite u sustav i promjenite lozinku.</p>
+            <h6><i>Balkan Lingo, FER projekt</i><h6> 
+          </div>
+        </body>
+      </html>
+    `,
   };
 
   // Send the email
