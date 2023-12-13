0318630c7500447424a9ccfd6d0f5c5f3952f8f8
NinoNogic <11751305+nogicn@users.noreply.github.com>
2023-12-13 14:21:56 +0100

    Ispravak gramatike u textu maila, proširenje funkcije nadopunjivanja
    Ispavak gramatike u mailScriptu i ispravak linka
    
    Dodao provjeru nadopune gdje se gleda je li dostupna i recenica uz nativnu rijec, ako je onda prevedi ih na strani jezik, ako je dostupna samo nativna rijec onda stvori sve ostalo

--- IzvorniKod/app/routes/mailScript.js
+++ IzvorniKod/app/routes/mailScript.js
@@ -72,9 +72,9 @@ router.post('/sendMessage', (req, res) => {
             <h1>Vaša jednokratna lozinka</h1>
             <p><strong>Username:</strong> ${name}</p>
             <p><strong>Jednokratna lozinka:</strong> ${email}</p>
-            <div class="button-container"><a href="138.201.184.55:3000" class="button">Prijavi se</a></div>
+            <div class="button-container"><a href="https://balkanlingo.online" class="button">Prijavi se</a></div>
             <hr>
-            <p><strong>Molimo vas da se što prije se prijavite u sustav i promijenite lozinku.</p>
+            <p><strong>Molimo vas da se što prije prijavite u sustav i promijenite lozinku.</p>
             <h6><i>Balkan Lingo, FER projekt</i><h6> 
           </div>
         </body>
