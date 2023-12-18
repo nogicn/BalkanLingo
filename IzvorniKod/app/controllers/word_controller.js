80826c2cef4e66272b6a587ea07a4721a92c9e93
NinoNogic <11751305+nogicn@users.noreply.github.com>
2023-12-18 20:09:11 +0100

    Dodao bazne testove i priprema za odvajanje podataka u env file
    Čiščenje logike i priprema za env varijable koje će diktirati ponašanje aplikacije
    
    Dodavanje testova koji se mogu pokrenuti sa npm run test

--- IzvorniKod/app/controllers/word_controller.js
+++ IzvorniKod/app/controllers/word_controller.js
@@ -160,34 +160,6 @@ async function editWord(req, res) {
         if (newPronunciation != word.pronunciation &&  word.pronunciation != "null") {
             fs.unlinkSync("./public/pronunciation/"+ word.pronunciation);
         }
-
-        /*if (newPronunciation != pronunciation && pronunciation == "null") {
-
-            let pronunciationFileName = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8) +".mp3";
-            let pronunciationFilePath = "./public/pronunciation/" + pronunciationFileName;
-
-            // Prepare options for fetch request
-            const voice = new ElevenLabs({
-                apiKey: "***REMOVED***", // Your API key
-                voiceId: "pNInz6obpgDQGcFmaJgB",             // Default Voice ID
-            });
-
-            let resource = await voice.textToSpeech({
-                // Required Parameters
-                fileName:        pronunciationFilePath,                    // The name of your audio file
-                textInput:       foreignWord,                // The text you wish to convert to speech
-            
-                // Optional Parameters
-                stability:       0.5,                            // The stability for the converted speech
-                similarityBoost: 0.5,                            // The similarity boost for the converted speech
-                modelId:         "eleven_multilingual_v2",   // The ElevenLabs Model ID
-                style:           1,                              // The style exaggeration for the converted speech
-                speakerBoost:    true                            // The speaker boost for the converted speech
-            }).then((res) => {
-                console.log(res);
-            });
-            pronunciation = pronunciationFileName;
-        }*/
         // update word in database
         let updateWord = db.prepare(wordModel.updateWord).run({wordId:id, foreignWord:foreignWord, foreignDescription:foreignDescription, nativeWord:nativeWord, nativeDescription:nativeDescription, pronunciation:newPronunciation});
         //console.log(updateWord);
