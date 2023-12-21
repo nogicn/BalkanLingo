26577bc344267c3f0c83577854dbfaf3d0feee06
NinoNogic <11751305+nogicn@users.noreply.github.com>
2023-12-21 11:12:16 +0100

    Uljepšanje koda sa Prettier formatiranjem
    Co-Authored-By: HrcoB <101458335+HrcoB@users.noreply.github.com>
    Co-Authored-By: Borna Krušlin <147040022+koderchina@users.noreply.github.com>

--- IzvorniKod/app/controllers/dictionary_controller.js
+++ IzvorniKod/app/controllers/dictionary_controller.js
@@ -1,138 +1,204 @@
-const db = require('../database/database');
-const dictionaryModel = require('../models/dictionary_model');
-const dictionaryUserModel = require('../models/dictionary_user_model');
-const userModel = require('../models/user_model');
-const wordModel = require('../models/word_model');
-const dictinoaryUserModel = require('../models/dictionary_user_model');
-const activeQuestionModel = require('../models/active_question_model');
-const languageModel = require('../models/language_model');
-const ejs = require('ejs');
+const db = require("../database/database");
+const dictionaryModel = require("../models/dictionary_model");
+const dictionaryUserModel = require("../models/dictionary_user_model");
+const userModel = require("../models/user_model");
+const wordModel = require("../models/word_model");
+const dictinoaryUserModel = require("../models/dictionary_user_model");
+const activeQuestionModel = require("../models/active_question_model");
+const languageModel = require("../models/language_model");
+const ejs = require("ejs");
 
-function dashboard (req, res) {
-    let dictionaries = [];
-    if (req.session.is_admin) {
-        dictionaries = db.prepare(dictionaryModel.getAllDictionariesWithIcons).all();
-    }else {
-        dictionaries = db.prepare(dictionaryModel.getDictionariesForUser).all({userId:req.session.user_id});
-    }
-    res.render('landingPage', { title: 'Express', dictionaries: dictionaries, is_admin: req.session.is_admin });
+function dashboard(req, res) {
+  let dictionaries = [];
+  if (req.session.is_admin) {
+    dictionaries = db
+      .prepare(dictionaryModel.getAllDictionariesWithIcons)
+      .all();
+  } else {
+    dictionaries = db
+      .prepare(dictionaryModel.getDictionariesForUser)
+      .all({ userId: req.session.user_id });
+  }
+  res.render("landingPage", {
+    title: "Express",
+    dictionaries: dictionaries,
+    is_admin: req.session.is_admin,
+  });
 }
 
-
 function getAllDictionaries(req, res) {
-    let dictionaries = [];
-    if (req.session.is_admin) {
-        res.redirect('/dashboard');
-    }else{
-        dictionaries = db.prepare(dictionaryModel.getDictionariesNotAssignedToUser).all({userId:req.session.user_id});
-    }
-    
-    res.render('addDictionary', { title: 'Add Dictionary', dictionaries: dictionaries });
+  let dictionaries = [];
+  if (req.session.is_admin) {
+    res.redirect("/dashboard");
+  } else {
+    dictionaries = db
+      .prepare(dictionaryModel.getDictionariesNotAssignedToUser)
+      .all({ userId: req.session.user_id });
+  }
+
+  res.render("addDictionary", {
+    title: "Add Dictionary",
+    dictionaries: dictionaries,
+  });
 }
 
 function searchDictionary(req, res) {
-    const id = req.params.id;
-    const allwords = db.prepare(wordModel.getWordByDictionaryId).all({dictionaryId:id});
-    const Dictionary = db.prepare(dictionaryModel.getDictionaryById).get({id:id});
-    res.render('dictSearch', { title: 'Search Dictionary', words: allwords, dictionary: Dictionary });
+  const id = req.params.id;
+  const allwords = db
+    .prepare(wordModel.getWordByDictionaryId)
+    .all({ dictionaryId: id });
+  const Dictionary = db
+    .prepare(dictionaryModel.getDictionaryById)
+    .get({ id: id });
+  res.render("dictSearch", {
+    title: "Search Dictionary",
+    words: allwords,
+    dictionary: Dictionary,
+  });
 }
 
 function removeDictionary(req, res) {
-    if (req.session.is_admin) {
+  if (req.session.is_admin) {
+    try {
+      const id = req.params.id;
+      const getAllWordsFromDictionary = db
+        .prepare(wordModel.getWordByDictionaryId)
+        .all({ dictionaryId: id });
+      for (let i = 0; i < getAllWordsFromDictionary.length; i++) {
+        const deleteWord = db
+          .prepare(activeQuestionModel.deleteActiveQuestionWordId)
+          .run({ wordId: getAllWordsFromDictionary[i].id });
+      }
 
-        try {
-            const id = req.params.id;
-            const getAllWordsFromDictionary = db.prepare(wordModel.getWordByDictionaryId).all({dictionaryId:id});
-            for (let i = 0; i < getAllWordsFromDictionary.length; i++) {
-                const deleteWord = db.prepare(activeQuestionModel.deleteActiveQuestionWordId).run({wordId:getAllWordsFromDictionary[i].id});
-            }
-            
-            const allwords = db.prepare(wordModel.deleteWordByDictionaryId).run({dictionaryId:id});
-            const allusers = db.prepare(userModel.getAllUsers).all();
-            for (let i = 0; i < allusers.length; i++) {
-                const result = db.prepare(dictionaryUserModel.deleteDictionaryFromUser).run({dictionaryId:id, userId:allusers[i].id});
-            }
-            const deleteDictionary = db.prepare(dictionaryModel.deleteDictionary).run({id:id});
-            res.redirect('/dashboard');
-        }catch (err) {
-            console.error(err);
-            res.status(500).send("Internal Server Error: " + err.message);
-            return;
-        }
-        
+      const allwords = db
+        .prepare(wordModel.deleteWordByDictionaryId)
+        .run({ dictionaryId: id });
+      const allusers = db.prepare(userModel.getAllUsers).all();
+      for (let i = 0; i < allusers.length; i++) {
+        const result = db
+          .prepare(dictionaryUserModel.deleteDictionaryFromUser)
+          .run({ dictionaryId: id, userId: allusers[i].id });
+      }
+      const deleteDictionary = db
+        .prepare(dictionaryModel.deleteDictionary)
+        .run({ id: id });
+      res.redirect("/dashboard");
+    } catch (err) {
+      console.error(err);
+      res.status(500).send("Internal Server Error: " + err.message);
+      return;
     }
-    else {
-       try {
-        const id = req.params.id;
-        const user = db.prepare(userModel.getUserByToken).get({token:req.session.token});
-        db.prepare(dictionaryUserModel.deleteDictionaryFromUser).run({dictionaryId:id, userId:user.id});
-        res.redirect('/dashboard');
-       }catch (err) {
-        console.error(err);
-        res.status(500).send("Internal Server Error: " + err.message);
-        return;
-       }
+  } else {
+    try {
+      const id = req.params.id;
+      const user = db
+        .prepare(userModel.getUserByToken)
+        .get({ token: req.session.token });
+      db.prepare(dictionaryUserModel.deleteDictionaryFromUser).run({
+        dictionaryId: id,
+        userId: user.id,
+      });
+      res.redirect("/dashboard");
+    } catch (err) {
+      console.error(err);
+      res.status(500).send("Internal Server Error: " + err.message);
+      return;
     }
+  }
 }
 
 function addDictionary(req, res) {
-    if (req.session.is_admin) {
-        res.redirect('/dictionary/adminAddDict');
-    }else{
-        // get user from db
-        const user = db.prepare(userModel.getUserByToken).get({token:req.session.token});
-        // get all dictionaries not assigned to user
-        const dictionaries = db.prepare(dictionaryModel.getDictionariesNotAssignedToUser).all({userId:user.id});
+  if (req.session.is_admin) {
+    res.redirect("/dictionary/adminAddDict");
+  } else {
+    // get user from db
+    const user = db
+      .prepare(userModel.getUserByToken)
+      .get({ token: req.session.token });
+    // get all dictionaries not assigned to user
+    const dictionaries = db
+      .prepare(dictionaryModel.getDictionariesNotAssignedToUser)
+      .all({ userId: user.id });
 
-        res.render('addDictionary', { title: 'Add Dictionary', dictionaries: dictionaries });
-    }
+    res.render("addDictionary", {
+      title: "Add Dictionary",
+      dictionaries: dictionaries,
+    });
+  }
 }
 
 function addDictionaryToUser(req, res) {
-    const id = req.params.id;
-    
-    const result = db.prepare(dictionaryUserModel.addDictionaryToUser).run({dictionaryId:id, userId:req.session.user_id});
-    res.redirect('/dashboard');
+  const id = req.params.id;
+
+  const result = db
+    .prepare(dictionaryUserModel.addDictionaryToUser)
+    .run({ dictionaryId: id, userId: req.session.user_id });
+  res.redirect("/dashboard");
 }
 
-function adminAddDict(req, res){
-    let languages = db.prepare(languageModel.getAllLanguages).all();
-    if(req.params.id === undefined){
-        res.render('dictionaryAddAdmin', { title: 'Add Dictionary', dictionary: {id:undefined, name:"", language_id:1, image_link:""}, languages:languages });
-    }
-    try {
-        const dictionary = db.prepare(dictionaryModel.getDictionaryById).get({id:req.params.id});
+function adminAddDict(req, res) {
+  let languages = db.prepare(languageModel.getAllLanguages).all();
+  if (req.params.id === undefined) {
+    res.render("dictionaryAddAdmin", {
+      title: "Add Dictionary",
+      dictionary: { id: undefined, name: "", language_id: 1, image_link: "" },
+      languages: languages,
+    });
+  }
+  try {
+    const dictionary = db
+      .prepare(dictionaryModel.getDictionaryById)
+      .get({ id: req.params.id });
 
-        res.render('dictionaryAddAdmin', { title: 'Edit Dictionary', dictionary: dictionary, languages:languages});
-    } catch (error) {
-        res.send(error);
-        return;
-    }
+    res.render("dictionaryAddAdmin", {
+      title: "Edit Dictionary",
+      dictionary: dictionary,
+      languages: languages,
+    });
+  } catch (error) {
+    res.send(error);
+    return;
+  }
 }
 
-function adminSaveDict(req, res){
-    try {
-        //hoce li ID biti ime rjecnika ili posebno da moze duplikat ime imati? za check prije updatea.
-        const dictionary = db.prepare(dictionaryModel.getDictionaryById).get({id:req.body.id});
-        if (dictionary === undefined) {
-            let res = db.prepare(dictionaryModel.createNewDictionary).run({name:req.body.description, language_id:req.body.language_id, imageLink:req.body.image_link});
-        }else{
-            let res = db.prepare(dictionaryModel.updateDictionary).run({name:req.body.description, language_id:req.body.language_id, imageLink:req.body.image_link, id:req.body.id});
-        }
-    } catch (error) {
-        res.send(error+" ");
-        return;
+function adminSaveDict(req, res) {
+  try {
+    //hoce li ID biti ime rjecnika ili posebno da moze duplikat ime imati? za check prije updatea.
+    const dictionary = db
+      .prepare(dictionaryModel.getDictionaryById)
+      .get({ id: req.body.id });
+    if (dictionary === undefined) {
+      let res = db
+        .prepare(dictionaryModel.createNewDictionary)
+        .run({
+          name: req.body.description,
+          language_id: req.body.language_id,
+          imageLink: req.body.image_link,
+        });
+    } else {
+      let res = db
+        .prepare(dictionaryModel.updateDictionary)
+        .run({
+          name: req.body.description,
+          language_id: req.body.language_id,
+          imageLink: req.body.image_link,
+          id: req.body.id,
+        });
     }
-    res.redirect('/dashboard');
+  } catch (error) {
+    res.send(error + " ");
+    return;
+  }
+  res.redirect("/dashboard");
 }
 
 module.exports = {
-    getAllDictionaries,
-    searchDictionary,
-    removeDictionary,
-    addDict: addDictionary,
-    addDictToUser: addDictionaryToUser,
-    adminAddDict,
-    adminSaveDict,
-    dashboard
-}
+  getAllDictionaries,
+  searchDictionary,
+  removeDictionary,
+  addDict: addDictionary,
+  addDictToUser: addDictionaryToUser,
+  adminAddDict,
+  adminSaveDict,
+  dashboard,
+};
--- IzvorniKod/app/controllers/users_controller.js
+++ IzvorniKod/app/controllers/users_controller.js
@@ -1,181 +1,201 @@
-const user = require('../models/user_model');
-const db = require('../database/database');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const user = require("../models/user_model");
+const db = require("../database/database");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 const salt = bcrypt.genSaltSync(10);
-var sendEmail = require('../middleware/mail_middleware'); 
+var sendEmail = require("../middleware/mail_middleware");
 
 // create a new user
 function createUser(req, res) {
-    // check if incoming request is coming from localhost
-    const { name, surname, email } = req.body;
-    try {
-        // create random string for password
-        const password = Math.random().toString(36).slice(-8);
-        //const hash = bcrypt.hashSync(password, salt);
-        const result = db.prepare(user.createUser).run({name:name, surname:surname, email:email, password:password});
-        if (result.changes !== 0) {
-            sendEmail(email, password);
-            res.render('resetPassNotif', { title: 'Register' });
-        }else{
-            res.redirect('/login');
-        }
-    } catch (err) {
-        console.error(err);
-        res.status(404).send("User not created " + err.message);
+  // check if incoming request is coming from localhost
+  const { name, surname, email } = req.body;
+  try {
+    // create random string for password
+    const password = Math.random().toString(36).slice(-8);
+    //const hash = bcrypt.hashSync(password, salt);
+    const result = db
+      .prepare(user.createUser)
+      .run({ name: name, surname: surname, email: email, password: password });
+    if (result.changes !== 0) {
+      sendEmail(email, password);
+      res.render("resetPassNotif", { title: "Register" });
+    } else {
+      res.redirect("/login");
     }
-    
+  } catch (err) {
+    console.error(err);
+    res.status(404).send("User not created " + err.message);
+  }
 }
 
 // login a user
 function loginUser(req, res) {
-    const { email, password } = req.body;
-    try {
-        var pass = db.prepare(user.getUserByEmail).get({email:email});
-        if (!pass) {
-            res.status(404).send("User not found");
+  const { email, password } = req.body;
+  try {
+    var pass = db.prepare(user.getUserByEmail).get({ email: email });
+    if (!pass) {
+      res.status(404).send("User not found");
+    }
+    pass = pass.password;
+    if (password != pass) {
+      const hash = bcrypt.hashSync(password, salt);
+      const row = db
+        .prepare(user.loginEmailPassword)
+        .get({ email: email, password: hash });
+      if (!row) {
+        res.status(404).send("User not found");
+      } else {
+        req.session.token = null;
+        const token = jwt.sign(
+          { mail: req.body.email },
+          "iamaverystrongsecretyesyes?"
+        );
+        const update = db
+          .prepare(user.updateTokenByEmail)
+          .get({ token: token, email: email });
+        if (update.changes !== 0) {
+          req.session.token = token;
+          req.session.email = email;
+          req.session.name = update.name;
+          req.session.surname = update.surname;
+          req.session.is_admin = update.is_admin == 1 ? true : false;
+          //res.json(update);
+          res.redirect("/dashboard");
         }
-        pass = pass.password;
-        if(password != pass) {
-            const hash = bcrypt.hashSync(password, salt);
-            const row = db.prepare(user.loginEmailPassword).get({email:email, password:hash});
-            if (!row) {
-                res.status(404).send("User not found");
-            } else {
-                req.session.token = null;
-                const token = jwt.sign({'mail':req.body.email}, 'iamaverystrongsecretyesyes?');
-                const update = db.prepare(user.updateTokenByEmail).get({token:token, email:email})
-                if (update.changes !== 0) {
-                    req.session.token = token;
-                    req.session.email = email;
-                    req.session.name = update.name;
-                    req.session.surname = update.surname;
-                    req.session.is_admin = update.is_admin == 1 ? true : false;
-                    //res.json(update);
-                    res.redirect('/dashboard');
-                }
-            }
-        } else {
-            res.render('createPass', {email:email});
-        }
-      } catch (err) {
-            console.error(err);
-            res.status(500).send("Internal Server Error: " + err.message);
       }
+    } else {
+      res.render("createPass", { email: email });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error: " + err.message);
+  }
 }
 
-
 function logoutUser(req, res) {
-    let checkToken = db.prepare(user.getUserByToken).get({ token: req.session.token });
+  let checkToken = db
+    .prepare(user.getUserByToken)
+    .get({ token: req.session.token });
 
-    if (checkToken.id == undefined) {
-        res.status(302).send("Error no token");
-        return;
+  if (checkToken.id == undefined) {
+    res.status(302).send("Error no token");
+    return;
+  }
+
+  let update = db
+    .prepare(user.updateTokenByEmail)
+    .run({ token: null, email: req.session.email });
+
+  if (update.changes == 0) {
+    res.status(302).send("Error");
+    return;
+  }
+
+  const cookies = req.cookies;
+
+  for (const cookieName in cookies) {
+    if (cookies.hasOwnProperty(cookieName)) {
+      // Set each cookie's expiration date to a date in the past
+      res.cookie(cookieName, "", { expires: new Date(0), path: "/" });
     }
-
-    let update = db.prepare(user.updateTokenByEmail).run({ token: null, email: req.session.email });
-
-    if (update.changes == 0) {
-        res.status(302).send("Error");
-        return;
-    }
-
-    const cookies = req.cookies;
-
-    for (const cookieName in cookies) {
-        if (cookies.hasOwnProperty(cookieName)) {
-        // Set each cookie's expiration date to a date in the past
-        res.cookie(cookieName, '', { expires: new Date(0), path: '/' });
-        }
-    }
-    req.session.destroy();
-    res.redirect('/');
+  }
+  req.session.destroy();
+  res.redirect("/");
 }
 //Get all users
 function getAllUsers(req, res) {
-    try {
-        const rows = db.prepare(user.getAllUsers).all();
-        result = JSON.stringify(rows);
-        res.json(result);
-    } catch (err) {
-        console.error(err);
-        res.status(404).send("Users not found "+ err);
-    }
+  try {
+    const rows = db.prepare(user.getAllUsers).all();
+    result = JSON.stringify(rows);
+    res.json(result);
+  } catch (err) {
+    console.error(err);
+    res.status(404).send("Users not found " + err);
+  }
 }
 
 function resetPwd(req, res) {
-    const {email} = req.body;
-    try {
-        const row = db.prepare(user.getUserByEmail).get({email:email});
-        if (!row) {
-            res.status(404).send("User not found");
-        } else { 
-            // create random string for password
-            const password = Math.random().toString(36).slice(-8);
-            const result = db.prepare(user.updatePasswordByEmail).run({email:email, password:password});
-            if (result.changes !== 0) {
-                sendEmail(email, password);
-                res.render('resetPassNotif', { title: 'Register' });
-            }else{
-                res.redirect('/login');
-            }
-        }
-      } catch (err) {
-            console.error(err);
-            res.status(500).send("Internal Server Error: " + err.message);
+  const { email } = req.body;
+  try {
+    const row = db.prepare(user.getUserByEmail).get({ email: email });
+    if (!row) {
+      res.status(404).send("User not found");
+    } else {
+      // create random string for password
+      const password = Math.random().toString(36).slice(-8);
+      const result = db
+        .prepare(user.updatePasswordByEmail)
+        .run({ email: email, password: password });
+      if (result.changes !== 0) {
+        sendEmail(email, password);
+        res.render("resetPassNotif", { title: "Register" });
+      } else {
+        res.redirect("/login");
       }
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error: " + err.message);
+  }
 }
 
 function editUser(req, res) {
-    const { name, surname } = req.body;
-    try {
-        const update = db.prepare(user.updateUserByToken).run({name:name, surname:surname, token:req.session.token})
-        if (update.changes !== 0) {
-            req.session.name = name;
-            req.session.surname = surname;
-            res.redirect('/user/edit');
-        }
-      } catch (err) {
-            console.error(err);
-            res.status(500).send("Internal Server Error: " + err.message);
-      }
+  const { name, surname } = req.body;
+  try {
+    const update = db
+      .prepare(user.updateUserByToken)
+      .run({ name: name, surname: surname, token: req.session.token });
+    if (update.changes !== 0) {
+      req.session.name = name;
+      req.session.surname = surname;
+      res.redirect("/user/edit");
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error: " + err.message);
+  }
 }
 
 function createPass(req, res) {
-    const {email, password, password2} = req.body;
-    try {
-        if (password != password2) {
-            res.status(404).send("Passwords do not match");
-        } else { 
-            const hash = bcrypt.hashSync(password, salt);
-            const result = db.prepare(user.updatePasswordByEmail).run({email:email, password:hash});
-            const row = db.prepare(user.loginEmailPassword).get({email:email, password:hash});
-            const token = jwt.sign({'mail':email}, 'iamaverystrongsecretyesyes?');
-            const update = db.prepare(user.updateTokenByEmail).get({token:token, email:email})
-                if (update.changes !== 0) {
-                    req.session.token = token;
-                    req.session.email = email;
-                    req.session.name = update.name;
-                    req.session.surname = update.surname;
-                    req.session.is_admin = update.is_admin == 1 ? true : false;
-                    //res.json(update);
-                    res.redirect('/dashboard');
-                }
-            
-        }
-      } catch (err) {
-            console.error(err);
-            res.status(500).send("Internal Server Error: " + err.message);
+  const { email, password, password2 } = req.body;
+  try {
+    if (password != password2) {
+      res.status(404).send("Passwords do not match");
+    } else {
+      const hash = bcrypt.hashSync(password, salt);
+      const result = db
+        .prepare(user.updatePasswordByEmail)
+        .run({ email: email, password: hash });
+      const row = db
+        .prepare(user.loginEmailPassword)
+        .get({ email: email, password: hash });
+      const token = jwt.sign({ mail: email }, "iamaverystrongsecretyesyes?");
+      const update = db
+        .prepare(user.updateTokenByEmail)
+        .get({ token: token, email: email });
+      if (update.changes !== 0) {
+        req.session.token = token;
+        req.session.email = email;
+        req.session.name = update.name;
+        req.session.surname = update.surname;
+        req.session.is_admin = update.is_admin == 1 ? true : false;
+        //res.json(update);
+        res.redirect("/dashboard");
       }
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error: " + err.message);
+  }
 }
 
 // export all functions
 module.exports = {
-    createUser,
-    loginUser,
-    getAllUsers,
-    logoutUser,
-    resetPwd,
-    editUser,
-    createPass
-}
+  createUser,
+  loginUser,
+  getAllUsers,
+  logoutUser,
+  resetPwd,
+  editUser,
+  createPass,
+};
