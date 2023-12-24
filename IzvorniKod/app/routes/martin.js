a6340f0e5f1e5d9d15308e74b29145bf05098e61
MartinWeb2003 <144782669+MartinWeb2003@users.noreply.github.com>
2023-12-24 11:53:54 +0100

    Popravio CSS za learnSession
    EJS i CSS za learnSession i writeWord su sada uskladjeni

--- IzvorniKod/app/routes/martin.js
+++ IzvorniKod/app/routes/martin.js
@@ -7,7 +7,7 @@ var dictionaryController = require('../controllers/dictionary_controller');
 /* GET home page. */
 router.get('/', function (req, res) {
         //res.render('sayWord', { title: 'Error' });
-        res.render("forOFor", { status: 500, errorText: "Greska kod spremanja riječnika!", link: "/dashboard" })
+        res.render("learnSession", { status: 500, errorText: "Greska kod spremanja riječnika!", link: "/dashboard" })
 
 });
 
