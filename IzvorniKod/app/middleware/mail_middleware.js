190f551b069a24eacff5353668d9ec16a23007de
Alberto Kerim <40634753+AlbertoKerim@users.noreply.github.com>
2023-12-28 12:46:05 +0100

    Stavljen dizajn HTML za slanje lozinke te ikona za administratore

--- IzvorniKod/app/middleware/mail_middleware.js
+++ IzvorniKod/app/middleware/mail_middleware.js
@@ -16,61 +16,48 @@ const sendEmail = (email, password) =>
     const mailOptions = {
       from: 'balkanlingo@gmail.com',
       to: email,
-      subject: 'Password set',
+      subject: 'Password reset',
       html: `
       <html>
-          <head>
-            <style>
-              body {
-                font-family: 'Arial', sans-serif;
-                background-color: #f4f4f4;
-              }
-              .container {
-                max-width: 600px;
-                margin: 20px auto;
-                padding: 20px;
-                background-color: #fff;
-                border-radius: 10px;
-                box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-              }
-              h1 {
-                color: #333;
-                text-align: center;
-              }
-              p {
-                color: #666;
-              }
-              h6 {
-                color: #4CA;
-              }
-              .button-container {
-                text-align: center;
-              }
-              .button {
-                display: inline-block;
-                padding: 10px 20px;
-                font-size: 16px;
-                text-align: center;
-                text-decoration: none;
-                background-color: #4CAF50;
-                color: #fff;
-                border-radius: 5px;
-                cursor: pointer;
-              }
-            </style>
-          </head>
-          <body>
-            <div class="container">
-              <h1>Vaša jednokratna lozinka</h1>
-              <p><strong>Email:</strong> ${email}</p>
-              <p><strong>Jednokratna lozinka:</strong> ${password}</p>
-              <div class="button-container"><a href="https://balkanlingo.online/login" class="button">Prijavi se</a></div>
-              <hr>
-              <p><strong>Molimo vas da se što prije prijavite u sustav i promijenite lozinku.</p>
-              <h6><i>Balkan Lingo, FER projekt</i><h6> 
+        <head></head>
+        <body style="font-family: 'Arial', sans-serif;
+          background-color: #f0f0f0; padding: 10%">
+          <div class="container" style="max-width: 600px;
+              margin: 20px auto;
+              padding: 20px;
+              background-color: #fff;
+              border-radius: 10px;
+              box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
+            <h1 style="color: #333;
+                  text-align: center;">Vaša jednokratna lozinka</h1>
+            <p style="color: #666;">
+              <strong>Email:</strong> ${email}
+            </p>
+            <img style="display:none">
+            <p style="color: #666;">
+              <strong>Jednokratna lozinka:</strong> ${password}
+            </p>
+            <div class="button-container" style="text-align: center;">
+              <a href="https://balkanlingo.online/login" class="button" style="display: inline-block;
+                  padding: 10px 20px;
+                  font-size: 16px;
+                  text-align: center;
+                  text-decoration: none;
+                  background-color: #4CAF50;
+                  color: #fff;
+                  border-radius: 5px;
+                  cursor: pointer;">Prijavi se</a>
             </div>
-          </body>
-        </html>
+            <hr>
+            <p style="color: #666;">
+              <strong>Molimo vas da se što prije prijavite u sustav i promijenite lozinku.</strong>
+            </p>
+            <h6 style="color: #4CA;">
+              <i>Balkan Lingo, FER projekt</i>
+              <h6>
+          </div>
+        </body>
+      </html>
       `,
     };
   
